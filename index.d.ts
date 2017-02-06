a33f5f99a6685dddfc25c12a04754010e587788f
Adam Venturella <aventurella@blitzagency.com>
2017-02-06 14:58:39 -0800

    Update type definition

--- index.d.ts
+++ index.d.ts
@@ -7,38 +7,45 @@
  */
 import { IContainer, IDomFactory }	from 'react-habitat';
 
+
 export interface IReduxContainer extends IContainer {
+	/**
+	 * Register a new component in the container
+	 * @param {string}      name        - The key that identifies this component
+	 * @param {*}           comp        - The component class
+	 */
+	register: (name: string, comp: any) => void;
 
-	constructor: (store: any) => IReduxContainer;
+	/**
+	 * Register multiple components in the container
+	 * @param {object}      comps        - The components
+	 */
+	registerAll: (comps: {}) => void;
 
+	/**
+	 * Get a registered component for a key
+	 * @param {string}      name        - The key name of the component that has been registered
+	 */
+	resolve: (name: string) => any;
+
+	/**
+	* The container's unique id
+	*/
+	id: () => string;
+
+	/**
+	 * The containers dom factory
+	 */
+	domFactory: () => IDomFactory;
 }
 
+export interface IReduxContainerConstructable{
+	new (store: any): IReduxContainer;
+}
 
 declare module ReduxHabitat {
-
-	export class Container implements IReduxContainer {
-
-		/**
-		 * The container id
-		 */
-		id: () => string;
-
-		/**
-		 * Register a component
-		 */
-		registerComponent: (name: string, comp: any) => void;
-
-		/**
-		 * Resolve a component
-		 */
-		resolve: (name: string) => any;
-
-		/**
-		 * The containers dom factory
-		 */
-		domFactory: () => IDomFactory;
-	}
-
+	export var Container: IReduxContainerConstructable;
 }
+
 
 export default ReduxHabitat;
